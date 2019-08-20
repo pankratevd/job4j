f26ccdcb1ec29e8ed0fbf53f4ed4d313eb66841e
pankratevd <d.pankratiev@gmail.com>
2019-08-20 08:54:14 +0300

    5.6. Ипотека.[#168455]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(int amount, int monthly, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            year++;
+            double amountWithPercent = amount*(1+percent/100);
+            amount = (int)amountWithPercent-monthly*12;
+        }
+        return year;
+    }
+}
