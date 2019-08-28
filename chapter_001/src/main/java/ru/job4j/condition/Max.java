285f6bff51382aa221880f5ac8ca3f85e110e378
pankratevd <d.pankratiev@gmail.com>
2019-08-28 15:41:17 +0300

    3. Перегрузить метод max для трех чисел[#168572]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -5,4 +5,14 @@ public class Max {
         int result = left >= right ? left : right;
         return result;
     }
+
+    public int max(int first, int second, int third) {
+        int result = third >= max(first, second) ? third : max(first, second);
+        return result;
+    }
+
+    public int max(int first, int second, int third, int fourth) {
+        int result = fourth >= max(first, second, third) ? fourth : max(first, second, third);
+        return result;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,27 +1,66 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
+import java.util.HashSet;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-    public class MaxTest {
-        @Test
-        public void whenMax1To2Then2() {
-            Max max = new Max();
-            int result = max.max(1, 2);
-            assertThat(result, is(2));
-        }
-        @Test
-        public void whenMax3To3Then3() {
-            Max max = new Max();
-            int result = max.max(3, 3);
-            assertThat(result, is(3));
-        }
-
-        @Test
-        public void whenMax5To3Then5() {
-            Max max = new Max();
-            int result = max.max(5, 3);
-            assertThat(result, is(5));
-        }
+public class MaxTest {
+    @Test
+    public void whenMax1To2Then2() {
+        Max max = new Max();
+        int result = max.max(1, 2);
+        assertThat(result, is(2));
     }
+
+    @Test
+    public void whenMax3To3Then3() {
+        Max max = new Max();
+        int result = max.max(3, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax5To3Then5() {
+        Max max = new Max();
+        int result = max.max(5, 3);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void when1to2to3Then3() {
+        Max max = new Max();
+        int result = max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void when3to2to1Then3() {
+        Max max = new Max();
+        int result = max.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void when1to2to3to4Then4() {
+        Max max = new Max();
+        int result = max.max(4, 1, 2, 3);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void when3to4to2to1Then4() {
+        Max max = new Max();
+        int result = max.max(3, 4, 2, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void when4to4to4to1Then4() {
+        Max max = new Max();
+        int result = max.max(4, 4, 4, 1);
+        assertThat(result, is(4));
+    }
+}
