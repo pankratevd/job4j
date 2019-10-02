b14e08ac975b4451282c436b716842fe74fbb73c
pankratevd <d.pankratiev@gmail.com>
2019-10-02 15:04:20 +0300

    Очередь с приоритетом на LinkedList[#168526]

---
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,25 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        if (tasks.size() == 0) {
+            tasks.add(task);
+            return;
+        }
+
+        while (index < tasks.size() && tasks.get(index).getPriority() < task.getPriority()) {
+            index++;
+        }
+        tasks.add(index, task);
+
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("middle1", 3));
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+
+    @Test
+    public void whenSameHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("middle1", 3));
+        queue.put(new Task("low", 3));
+        queue.put(new Task("middle", 3));
+        queue.put(new Task("urgent", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+
+    @Test
+    public void whenOneTask() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("urgent", 1));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+
+    @Test
+    public void whenHigerProrityTaskAtBegin() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("low", 3));
+        queue.put(new Task("low1", 3));
+        queue.put(new Task("low2", 5));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+    @Test
+    public void whenHigerProrityTaskAtEnd() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("low1", 4));
+        queue.put(new Task("low2", 3));
+        queue.put(new Task("urgent", 1));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+    @Test
+    public void whenTwoTasks() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 2));
+        queue.put(new Task("urgent", 1));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+}
+
