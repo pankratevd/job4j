d21facaea3b593efaecf3980a0d86dc760d5a0af
pankratevd <d.pankratiev@gmail.com>
2019-10-04 10:28:00 +0300

    1. Организовать сортировку User[#168517]

---
+++ chapter_002/src/main/java/ru/job4j/compare/SortUser.java
@@ -0,0 +1,11 @@
+package ru.job4j.compare;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SortUser {
+    public Set<User> sort(List<User> list) {
+        return new TreeSet<User>(list);
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/compare/SortUserTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.compare;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SortUserTest {
+
+    @Test
+    public void checkSort() {
+        List<User> list = new ArrayList<>();
+        SortUser sortUser = new SortUser();
+        User user1 = new User("ivan1", 25);
+        User user2 = new User("ivan2", 27);
+        User user3 = new User("ivan3", 21);
+        User user4 = new User("ivan4", 20);
+        User user5 = new User("ivan5", 30);
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        list.add(user5);
+        ArrayList<User> expected = new ArrayList<>();
+        expected.add(user4);
+        expected.add(user3);
+        expected.add(user1);
+        expected.add(user2);
+        expected.add(user5);
+        Set<User> result = sortUser.sort(list);
+
+        assertThat(expected, is(new ArrayList<>(result)));
+
+    }
+
+}
