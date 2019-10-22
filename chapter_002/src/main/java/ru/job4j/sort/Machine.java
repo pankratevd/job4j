f8c17e988eafd5242a77f4dad07b7d94f408b9cc
pankratevd <d.pankratiev@gmail.com>
2019-10-22 15:23:48 +0300

    changed style - int coins

--- chapter_002/src/main/java/ru/job4j/sort/Machine.java
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -3,19 +3,19 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int i = 0; i < COINS.length; i++) {
+        for (int i = 0; i < coins.length; i++) {
             if (change == 0) {
                 break;
             }
-            while (change >= COINS[i]) {
-                rsl[size++] = COINS[i];
-                change -= COINS[i];
+            while (change >= coins[i]) {
+                rsl[size++] = coins[i];
+                change -= coins[i];
             }
         }
         return Arrays.copyOf(rsl, size);
