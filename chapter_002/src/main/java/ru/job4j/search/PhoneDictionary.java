06477a71123fa507b2ac674706d473b70e1cbc2c
pankratevd <d.pankratiev@gmail.com>
2019-11-08 15:19:38 +0300

    Local-Variable Type Inference[#168420]

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PhoneDictionary {
-    private List<Person> persons = new ArrayList<Person>();
+    private List<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
@@ -17,7 +17,7 @@ public class PhoneDictionary {
      */
     public List<Person> find(String key) {
         List<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (person.getAddress().contains(key) || person.getName().contains(key) || person.getPhone().contains(key) || person.getSurname().contains(key)) {
                 result.add(person);
             }
