6b35d72393c7b841b9c573b990851ea15e7c8cc8
pankratevd <d.pankratiev@gmail.com>
2019-11-21 11:47:01 +0300

    5.2.1. Реализовать SimpleArray<T>[#199393]

---
+++ junior/jchapter_001/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,80 @@
+package ru.job4j.generic;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable<T> {
+
+    private T[] array;
+
+    private int position = 0;
+
+    private int itPosition = 0;
+
+    public SimpleArray(final int size) {
+        array = (T[]) new Object[size];
+    }
+
+    public void add(T model) {
+        if (this.position >= array.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        array[this.position++] = model;
+    }
+
+    public void set(int index, T model) {
+        if (index >= this.position) {
+            throw new IndexOutOfBoundsException();
+        }
+        array[index] = model;
+    }
+
+    public void remove(int index) {
+        if (index >= this.position) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == this.array.length - 1) {
+            array[index] = null;
+            this.position--;
+        } else {
+            System.arraycopy(this.array, index + 1, this.array, index, this.array.length - 1 - index);
+            this.array[--this.position] = null;
+        }
+    }
+
+    public T get(int index) {
+        if (index >= this.position) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            boolean isNext = false;
+            @Override
+            public boolean hasNext() {
+                return itPosition < position;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                isNext = true;
+                return array[itPosition++];
+            }
+            @Override
+            public void remove() {
+                if (!isNext) {
+                    throw new IllegalStateException();
+                }
+                isNext = false;
+                SimpleArray.this.remove(itPosition);
+            }
+        };
+    }
+}
---
+++ junior/jchapter_001/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -0,0 +1,223 @@
+package ru.job4j.generic;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleArrayTest {
+
+    @Test
+    public void whenAddThreeStringElementsThenSecondElementIsRight() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        array.add("string2");
+        array.add("string3");
+        String expected = "string2";
+        assertThat(array.get(1), is(expected));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddMoreElementsThenIndexThenException() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        array.add("string2");
+        array.add("string3");
+        array.add("string4");
+    }
+
+    @Test
+    public void getElementWhenExist() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        array.add("string2");
+        array.add("string3");
+        String expected = "string2";
+        assertThat(array.get(1), is(expected));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getElementWhenNotExistThenException() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        String expected = "string1";
+        assertThat(array.get(0), is(expected));
+        array.get(1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenRemoveNotExistThenException() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.remove(0);
+
+    }
+
+    @Test
+    public void whenSetSecondElementThenSecondIsChange() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        array.add("string2");
+        array.add("string3");
+        String expectedBefore = "string2";
+
+        assertThat(array.get(1), is(expectedBefore));
+
+        array.set(1, "stringChanged");
+        String expectedAfter = "stringChanged";
+
+        assertThat(array.get(1), is(expectedAfter));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenSetOutOfIndexThenException() {
+        SimpleArray<String> array = new SimpleArray<>(5);
+        array.add("string0");
+        array.set(1, "string");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenRemoveSecondElementInMiddle() {
+        SimpleArray<Integer> array = new SimpleArray<>(5);
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.remove(2);
+        Integer expected0 = 0;
+        Integer expected1 = 1;
+        Integer expected2 = 3;
+        Integer expected3 = 4;
+        Integer expected4 = null;
+        assertThat(array.get(0), is(expected0));
+        assertThat(array.get(1), is(expected1));
+        assertThat(array.get(2), is(expected2));
+        assertThat(array.get(3), is(expected3));
+        assertThat(array.get(4), is(expected4));
+
+        array.add(5);
+        assertThat(array.get(4), is(5));
+
+        array.add(6);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenRemovedIndexMoreThenExistThenException() {
+        SimpleArray<Object> array = new SimpleArray<>(3);
+
+        array.add(new Object());
+        array.add(new Object());
+        array.remove(3);
+
+    }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenRemoveTheLastElement() {
+        SimpleArray<Integer> array = new SimpleArray<>(5);
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+
+        array.remove(4);
+
+        Integer expected0 = 0;
+        Integer expected1 = 1;
+        Integer expected2 = 2;
+        Integer expected3 = 3;
+        Integer expected4 = null;
+        assertThat(array.get(0), is(expected0));
+        assertThat(array.get(1), is(expected1));
+        assertThat(array.get(2), is(expected2));
+        assertThat(array.get(3), is(expected3));
+        assertThat(array.get(4), is(expected4));
+
+        array.add(5);
+        assertThat(array.get(4), is(5));
+
+        array.add(6);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenRemoveTheFirstElement() {
+        SimpleArray<Integer> array = new SimpleArray<>(5);
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+
+        array.remove(0);
+
+        Integer expected0 = 1;
+        Integer expected1 = 2;
+        Integer expected2 = 3;
+        Integer expected3 = 4;
+        Integer expected4 = null;
+        assertThat(array.get(0), is(expected0));
+        assertThat(array.get(1), is(expected1));
+        assertThat(array.get(2), is(expected2));
+        assertThat(array.get(3), is(expected3));
+        assertThat(array.get(4), is(expected4));
+
+        array.add(5);
+        assertThat(array.get(4), is(5));
+
+        array.add(6);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testsIterateThroughCollection() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        array.add("string2");
+        array.add("string3");
+        Iterator<String> it = array.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("string1"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("string2"));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("string3"));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void testRemoveIteratorWhenPreviousIsNext() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        array.add("string2");
+        array.add("string3");
+
+        Iterator<String> it = array.iterator();
+
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("string1"));
+        it.remove();
+        assertThat(it.next(), is("string3"));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testRemoveIteratorWhenDoubleRemoveThenException() {
+        SimpleArray<String> array = new SimpleArray<>(3);
+        array.add("string1");
+        array.add("string2");
+        array.add("string3");
+
+        Iterator<String> it = array.iterator();
+
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("string1"));
+        it.remove();
+        it.remove();
+    }
+
+
+}
