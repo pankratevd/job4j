4c409d9149d760a840c2aa7b74a4f2585af6de44
pankratevd <d.pankratiev@gmail.com>
2019-12-02 09:12:42 +0300

    5.3.2. Создать контейнер на базе связанного списка [#199372]

---
+++ junior/jchapter_001/src/main/java/ru/job4j/list/DynamicLinkedList.java
@@ -0,0 +1,100 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicLinkedList<E> implements Iterable<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> first;
+    private Node<E> last;
+
+    public void add(E value) {
+        if (size == 0) {
+            Node<E> node = new Node<>(null, value, null);
+            first = node;
+            last = node;
+            size++;
+            modCount++;
+        } else {
+            Node<E> oldLast = last;
+            Node<E> node = new Node<>(first, value, null);
+            last = node;
+            oldLast.next = last;
+            size++;
+            modCount++;
+        }
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<E> result = first;
+        if (index > 0) {
+            for (int i = 0; i < index; i++) {
+                result = result.next;
+            }
+        }
+
+        return result.item;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int modCountExpected = modCount;
+            Node<E> it = first;
+
+            @Override
+            public boolean hasNext() {
+                checkModify();
+                return it != null;
+            }
+
+            @Override
+            public E next() {
+                checkModify();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                Node<E> result = it;
+
+                if (result != first) {
+                    result = it;
+                    it = it.next;
+                } else {
+                    it = it.next;
+                }
+
+                return result.item;
+            }
+
+            private void checkModify() {
+                if (modCountExpected != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
---
+++ junior/jchapter_001/src/test/java/ru/job4j/list/DynamicLinkedListTest.java
@@ -0,0 +1,82 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class DynamicLinkedListTest {
+
+
+    private DynamicLinkedList<Integer> list;
+
+    Iterator<Integer> it;
+
+    @Before
+    public void beforeTest() {
+        list = new DynamicLinkedList<Integer>();
+
+
+        for (int i = 0; i < 8; i++) {
+            list.add(i);
+        }
+
+        it = list.iterator();
+    }
+
+    @Test
+    public void checkGetWhenExist() {
+        assertThat(list.getSize(), is(8));
+        assertThat(list.get(0), is(0));
+        assertThat(list.get(1), is(1));
+        assertThat(list.get(2), is(2));
+        assertThat(list.get(3), is(3));
+        assertThat(list.get(4), is(4));
+        assertThat(list.get(5), is(5));
+        assertThat(list.get(6), is(6));
+        assertThat(list.get(7), is(7));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void checkWhenListContainsOneElement() {
+        list = new DynamicLinkedList<>();
+        list.add(0);
+        assertThat(list.getSize(), is(1));
+        assertThat(list.get(0), is(0));
+        list.get(1);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorWithoutChangeContainer() {
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(0));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(1));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(2));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(3));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(4));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(5));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(6));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(7));
+        assertFalse(it.hasNext());
+        it.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenModifiedAndIteratorHasNextThenException() {
+        assertTrue(it.hasNext());
+        list.add(8);
+        it.hasNext();
+    }
+}
