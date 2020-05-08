5dbef47c3e11858d2765c4e8be48fc4881dbbf42
pankratevd <d.pankratiev@gmail.com>
2020-05-08 12:36:12 +0300

    Видео [#199417]

---
+++ junior/jchapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (true) {
+                String first;
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str = in.readLine();
+                    first = str;
+                    while (!str.isEmpty()) {
+                        System.out.println(str);
+                        str = in.readLine();
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                }
+                if (first.contains("/?msg=Buy")) {
+                    break;
+                }
+            }
+        }
+    }
+}
