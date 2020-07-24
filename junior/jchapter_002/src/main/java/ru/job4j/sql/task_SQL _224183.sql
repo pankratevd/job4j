2dcc007101e2b9ee222a204fc095e5f3dd1592c0
pankratevd <d.pankratiev@gmail.com>
2020-07-24 10:51:21 +0300

    SQL [#224183]

---
+++ junior/jchapter_002/src/main/java/ru/job4j/sql/task_SQL _224183.sql
@@ -0,0 +1,67 @@
+CREATE TABLE meeting (
+	id serial PRIMARY KEY,
+	name VARCHAR(100)
+);
+
+CREATE TABLE users (
+	id serial PRIMARY KEY,
+	name VARCHAR(100)
+);
+
+CREATE TABLE userstatus (
+	id serial PRIMARY KEY,
+	name VARCHAR(100)
+);
+
+
+CREATE TABLE meeting_users_userstatus (
+	meeting_id int REFERENCES meeting(id),
+	user_id int REFERENCES users(id),
+	userstatus_id int REFERENCES userstatus(id),
+	UNIQUE (meeting_id, user_id)
+	);
+
+
+
+INSERT INTO users (name) VALUES 
+	('Ivan'),
+	('Mike'),
+	('Alex'),
+	('Andrew');
+
+
+INSERT INTO userstatus (name) VALUES 
+	('approved'),
+	('rejected');
+	
+INSERT INTO meeting (name) VALUES 
+	('meet1'),
+	('meet2'),
+	('meet3');
+
+
+INSERT INTO meeting_users_userstatus (meeting_id, user_id, userstatus_id) VALUES 
+(1, 1, 1),
+(1, 2, 2),
+(2, 2, 1),
+(2, 3, 1);
+
+
+-- Список всех встреч и количество подтвердивших участников.
+
+select m.name, count(mus.user_id) 
+from meeting as m
+join meeting_users_userstatus as mus ON m.id = mus.meeting_id
+join userstatus as us ON mus.userstatus_id = us.id
+WHERE us.name = 'approved'
+GROUP BY (m.name);
+
+
+
+-- все совещания, где не было ни одной заявки на посещения
+
+select m.name from meeting as m
+left join meeting_users_userstatus as mus ON m.id = mus.meeting_id
+WHERE mus.meeting_id IS NULL;
+
+
