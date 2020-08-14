fc0838ae7598bc214da79bf0aa03d6628f3277f6
Dmitrii <d.pankratiev@gmail.com>
2020-08-14 10:41:37 +0300

    Запуск нити. Thread#start() [#339949]

---
+++ chapter_007/src/main/java/concurrent/ConcurrentlyOutput.java
@@ -0,0 +1,12 @@
+package concurrent;
+
+public class ConcurrentlyOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        another.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
