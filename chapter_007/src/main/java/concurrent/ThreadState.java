f80242fb213b9e155f9d84b0f02b3e097d2c3273
Dmitrii <d.pankratiev@gmail.com>
2020-08-14 10:54:48 +0300

    Состояние нити. [#339946]

---
+++ chapter_007/src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,25 @@
+package concurrent;
+
+import javax.swing.table.TableRowSorter;
+
+public class ThreadState {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        Thread second = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+        });
+
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        System.out.println("Done");
+
+    }
+
+}
