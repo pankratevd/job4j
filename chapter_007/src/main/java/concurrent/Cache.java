d1107709dc25995f51732d68cb66593401ae8d93
Dmitrii <d.pankratiev@gmail.com>
2020-08-20 10:23:56 +0300

    Синхронизация общих ресурсов. [#339956]

---
+++ chapter_007/src/main/java/concurrent/Cache.java
@@ -0,0 +1,13 @@
+package concurrent;
+
+public class Cache {
+
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
