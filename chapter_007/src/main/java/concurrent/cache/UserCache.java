371a762d734bd3125ef7ab0e3435ff124ebc7885
pankratevd <d.pankratiev@gmail.com>
2020-08-24 13:06:36 +0300

    Thread без общих ресурсов [#339955]

---
+++ chapter_007/src/main/java/concurrent/cache/UserCache.java
@@ -0,0 +1,25 @@
+package concurrent.cache;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> result = new ArrayList<>();
+        users.forEach((k, v) -> result.add(User.of(v.getName())));
+        return result;
+    }
+}
