525c83fe210eb5ceca2e8a735b17e923dd70ac85
pankratevd <d.pankratiev@gmail.com>
2020-08-24 09:12:47 +0300

    Immutable объекты [#339954]

---
+++ chapter_007/src/main/java/concurrent/Node.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+import net.jcip.annotations.Immutable;
+
+@Immutable
+public class Node<T> {
+    private final Node next;
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
