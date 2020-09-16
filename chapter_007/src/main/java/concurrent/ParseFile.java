465811735b1d7fc930744ead6010e9f661850466
pankratevd <d.pankratiev@gmail.com>
2020-09-16 17:10:47 +0300

    Visibility. Общий ресурс вне критической секции [#339960]

--- chapter_007/src/main/java/concurrent/ParseFile.java
+++ chapter_007/src/main/java/concurrent/ParseFile.java
@@ -14,34 +14,30 @@ public class ParseFile {
     }
 
     public synchronized String getContent() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            int data = br.read();
-            while (data != -1) {
-                sb.append((char) data);
-                data = br.read();
-            }
-        }
-        return sb.toString();
+        return readContent(-1);
     }
 
     public synchronized String getContentWithoutUnicode() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            int data = br.read();
-            while (data != -1) {
-                if (data < 0x80) {
-                    sb.append((char) data);
-                }
-                data = br.read();
-            }
-        }
-        return sb.toString();
+        return readContent(0x80);
     }
 
     public synchronized void saveContent(String content) throws IOException {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
             bw.write(content);
         }
+    }
+
+    private synchronized String readContent(int code) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            int data = br.read();
+            while (data != -1) {
+                if (code == -1 || data <= code) {
+                    sb.append((char) data);
+                }
+                data = br.read();
+            }
+        }
+        return sb.toString();
     }
 }
