43e12564011d7d3256533390c92779ca5e4b9723
pankratevd <d.pankratiev@gmail.com>
2020-09-21 13:01:19 +0300

    Управление нитью через wait. [#339943]

---
+++ chapter_007/src/main/java/wait/CountBarrier.java
@@ -0,0 +1,31 @@
+package wait;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() throws InterruptedException {
+        synchronized (monitor) {
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count !=total) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
