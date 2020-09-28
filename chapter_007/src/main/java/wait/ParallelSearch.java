4ef3720f85da54175376426b0ee632bd290e10e9
pankratevd <d.pankratiev@gmail.com>
2020-09-28 10:45:17 +0300

    Обеспечить остановку потребителя. [#339940]

---
+++ chapter_007/src/main/java/wait/ParallelSearch.java
@@ -0,0 +1,35 @@
+package wait;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread consumer = new Thread(
+                () -> {
+
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        if (!queue.isEmpty()) {
+                            System.out.println(queue.poll());
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+    }
+}
