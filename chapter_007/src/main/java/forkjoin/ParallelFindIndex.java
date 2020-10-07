e5763c5074e6ea0a95efed56c76eddabbc84891b
pankratevd <d.pankratiev@gmail.com>
2020-10-07 15:26:57 +0300

    ForkJoin [#339937]

--- chapter_007/src/main/java/forkjoin/ParallelFindIndex.java
+++ chapter_007/src/main/java/forkjoin/ParallelFindIndex.java
@@ -1,52 +1,62 @@
 package forkjoin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
-public class ParallelFindIndex<T> extends RecursiveAction {
+public class ParallelFindIndex<T> {
+    private ForkJoinPool forkJoinPool;
+    int result;
 
-    private final T[] arr;
-    private final T obj;
-    private final int start;
-    private final int end;
-    static int result = -1;
-    static List<Integer> list = new ArrayList<>();
-
-    public ParallelFindIndex(T[] arr, T obj, int start, int end) {
-        this.arr = arr;
-        this.obj = obj;
-        this.start = start;
-        this.end = end;
+    public ParallelFindIndex() {
+        this.forkJoinPool = ForkJoinPool.commonPool();
     }
 
-    @Override
-    protected void compute() {
-        //  if (result != -1) return;
-        if (end - start < 100) {
-            process(start, end);
-        } else {
+    public ParallelFindIndex(int number) {
+        this.forkJoinPool = new ForkJoinPool(number);
+    }
 
-            int mid = (start + end) / 2;
-            ParallelFindIndex<T> p1 = new ParallelFindIndex<T>(arr, obj, start, mid);
-            ParallelFindIndex<T> p2 = new ParallelFindIndex<>(arr, obj, mid + 1, end);
-            p1.fork();
-            p2.fork();
-            p1.join();
-            p2.join();
+    public int getIndex(T[] arr, T obj) {
+        result = -1;
+        ParallelFindIndexInner array = new ParallelFindIndexInner(arr, obj, 0, arr.length - 1);
+        forkJoinPool.invoke(array);
+        return result;
+    }
+
+    class ParallelFindIndexInner extends RecursiveAction {
+
+        private final T[] arr;
+        private final T obj;
+        private final int start;
+        private final int end;
+
+        private ParallelFindIndexInner(T[] arr, T obj, int start, int end) {
+            this.arr = arr;
+            this.obj = obj;
+            this.start = start;
+            this.end = end;
         }
-    }
 
-    private int process(int start, int end) {
-        for (int i = start; i <= end; i++) {
-            //      System.out.printf("Search from %s to %s \n", start, end);
-            if (obj.equals(arr[i])) {
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!! FOUND !!!!!!!!!!!!!!!!!!!!");
-                //result = i;
-                list.add(i);
-
+        @Override
+        protected void compute() {
+            if ((end - start) < 10) {
+                process(start, end);
+            } else {
+                int mid = (start + end) / 2;
+                ParallelFindIndexInner p1 = new ParallelFindIndexInner(arr, obj, start, mid);
+                ParallelFindIndexInner p2 = new ParallelFindIndexInner(arr, obj, mid + 1, end);
+                p1.fork();
+                p2.fork();
+                p1.join();
+                p2.join();
             }
         }
-        return result;
+
+        private void process(int start, int end) {
+            for (int i = start; i <= end; i++) {
+                if (obj.equals(arr[i])) {
+                    result = i;
+                }
+            }
+        }
     }
 }
--- chapter_007/src/test/java/forkjoin/ParallelFindIndexTest.java
+++ chapter_007/src/test/java/forkjoin/ParallelFindIndexTest.java
@@ -2,57 +2,35 @@ package forkjoin;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ForkJoinPool;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ParallelFindIndexTest {
     @Test
-    public void whenObjectExistThenIndex199() {
-
-        Integer[] arr = new Integer[1000000];
-        for (int i = 0; i < arr.length; i++) {
+    public void whenObjectExists() {
+        int count = 10000;
+        Integer[] arr = new Integer[count];
+        for (int i = 0; i < count; i++) {
             arr[i] = i;
         }
-        ForkJoinPool forkJoinPool = new ForkJoinPool(4);
 
-
-        //for (int i = 0; i < 10000; i++) {
-        int i = 999999;
-        arr[23432] = 999999;
-        long s = System.currentTimeMillis();
-        ParallelFindIndex<Integer> array = new ParallelFindIndex<>(arr, i, 0, arr.length - 1);
-        System.out.println("Start");
-        forkJoinPool.invoke(array);
-        //   Assert.assertThat(ParallelFindIndex.result, is(i));
-        // }
-
-        long e = System.currentTimeMillis();
-        //System.out.println("Result: " + ParallelFindIndex.result);
-
-        System.out.println((e - s));
-        System.out.println(ParallelFindIndex.list);
-
-        List<Integer> list = Arrays.asList(arr);
-        s = System.currentTimeMillis();
-        //   for (int i = 0; i < 10000; i++) {
-    //    Assert.assertThat(list.indexOf(i), is(i));
-        //   }
-        System.out.println(list.indexOf(i));
-        e = System.currentTimeMillis();
-
-        System.out.println((e - s));
+        ParallelFindIndex<Integer> parallelFindIndex = new ParallelFindIndex<>();
+        for (int i = 0; i < count; i++) {
+            assertThat(parallelFindIndex.getIndex(arr, i), is(i));
+        }
     }
 
     @Test
-    public void whenNoObject() {
-        Integer[] arr = new Integer[10000];
-        for (int i = 0; i < arr.length; i++) {
+    public void whenNoObjectExists() {
+        int count = 10000;
+        Integer[] arr = new Integer[count];
+        for (int i = 0; i < count; i++) {
             arr[i] = i;
         }
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
-        forkJoinPool.invoke(new ParallelFindIndex<>(arr, 999999999, 0, arr.length - 1));
-        // Assert.assertThat(ParallelFindIndex.result, is(-1));
 
+        ParallelFindIndex<Integer> parallelFindIndex = new ParallelFindIndex<>();
+        for (int i = 0; i < count; i++) {
+            assertThat(parallelFindIndex.getIndex(arr, count + i), is(-1));
+        }
     }
 }
